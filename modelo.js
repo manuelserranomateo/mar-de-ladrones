d33d73d535f61fcb09ec0a3d5b00804d2e59bc8d
manuelserranomateo <manuelserranomateo@gmail.com>
2022-09-20 09:38:29 +0200

    Nuevos modelos y pruebas

--- modelo.js
+++ modelo.js
@@ -1,14 +1,33 @@
 function Juego(){
-    this.partidas = []; // [] array {} array asociativo(diccionario)
-    this.agregarPartida = function(nombre){
-        this.partidas.push(new Partida(nombre))
-    } 
-    this.eliminarPartida = function(nombre){
-        // TODO
+    this.partidas = {}; // [] array {} array asociativo(diccionario)
+    this.usuarios = {};
+
+    this.agregarUsuario = function(nick){
+        if (!this.usuarios[nick]){
+            this.usuarios[nick] = new Usuario(nick, this); // con el this le paso el objeto de Juego
+        }
+    }
+
+    this.eliminarUsuario = function(nick){
+        delete this.usuarios[nick];
+    }
+    
+    this.crearPartida = function(nick){
+        // obtener codigo unico
+        // crear la partida con propietario nick
+        // devolver el codigo
+        console.log('partida creada');
     }
 }
 
+function Usuario(nick, juego){
+    this.nick = nick; // Setter
+    this.juego = juego;
+    this.crearPartida = function(){
+        this.juego.crearPartida(this.nick);
+    }
+}
 
-function Partida(nombre){
-    this.nombre = nombre;
+function Partida(){
+    this.codigo = codigo;
 }
