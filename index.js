96b73e5569bf7d81e8c46b20021250810ac846d2
manuelserranomateo <manuelserranomateo@gmail.com>
2022-09-27 09:39:44 +0200

    Implementacion basica del API REST

--- index.js
+++ index.js
@@ -3,7 +3,16 @@ const fs = require('fs')
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res) => {
+/*  http get post put delete (se llaman verbos)
+    get "/"
+    get "/obtenerPartidas"
+    post "/agregarUsuario/:nick" post para enviar mucha informacion
+    put "/actualizarPartida"     get si envias poca informacion
+    delete "/eliminarPartida"   
+    ... etc
+    Son las distintas rutas con los parametros que requiera la logica
+*/
+app.get('/', (req, res) => { 
   res
     .status(200)
     .send("Hola")
@@ -11,9 +20,9 @@ app.get('/', (req, res) => {
 });
 
 // Start the server
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 3000; // Util para el despliegue process.env.PORT por si el OS tiene una variable definida
 
-app.listen(PORT, () => {
+app.listen(PORT, () => { // funcion de callback, 
   console.log(`App listening on port ${PORT}`);
   console.log('Press Ctrl+C to quit.');
 });
