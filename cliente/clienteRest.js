4636e800b52ea8c066fdda0b2684c93f92025f1c
Manuel Serrano <manuelserranomateo@gmail.com>
2022-10-10 15:32:01 +0200

    controlWeb y empezando con Crear Partida

--- cliente/clienteRest.js
+++ cliente/clienteRest.js
@@ -1,47 +1,50 @@
-function ClienteRest() { // ClienteRest es una funcion global
-    this.nick;
-    this.agregarUsuario = function (nick) {
-        var cli = this; // tipico de JS, se guarda una referencia el objeto para no perder el contexto
-        $.getJSON("/agregarUsuario/" + nick, function (data) { // peticion asincrona al servidor
-            // el data es lo que contesta el servidor, la proporciona el API REST, el response.send de esta ruta
-            // se ejecuta cuando conteste el servidor, funcion de callback
-            console.log(data);
-            if (data.nick != -1) {
-                console.log('Usuario ' + data.nick + ' registrado');
-                //ws.nick = data.nick;
-                //$.cookie("nick",ws.nick);
-                //iu.mostrarHome(data);
-            }
-            else {
-                console.log('El nick ya esta en uso');
-                //iu.mostrarModal("El nick ya está en uso");
-                //iu.mostrarAgregarJugador();
-            }
-        });
-        // cualquier cosa a partir de aqui, se ejecuta A LA VEZ que la llamada
-        // no se puede asumir que el servidor haya contestado, no deben depender
-    }
-
-    this.crearPartida = function (nick) {
-        var cli = this;
-        $.getJSON("/crearPartida/" + nick, function (data) {
-            this.nick = nick;
-            console.log(data);
-            if (data.codigo != -1) {
-                console.log('Usuario ' + nick + ' crea la partida ' + data.codigo);
-            }
-            else {
-                console.log('No se ha podido crear la partida ya que el usuario ' + nick + ' no esta registrado');
-            }
-        });
-    }
-    this.unirseAPartida=function(nick,codigo){
+function ClienteRest(){
+	this.nick;	
+	this.agregarUsuario=function(nick){
+		let cli=this;
+		$.getJSON("/agregarUsuario/"+nick,function(data){
+			//se ejecuta cuando conteste el servidor
+			console.log(data);
+			if (data.nick!=-1){
+				console.log("Usuario "+data.nick+" registrado")
+				cli.nick=data.nick;
+				//ws.nick=data.nick;
+				//$.cookie("nick",ws.nick);
+				iu.mostrarHome();
+				iu.mostrarCrearPartida();
+			}
+			else{
+				console.log("No se ha podido registrar el usuario")
+				//iu.mostrarModal("El nick ya está en uso");
+				iu.mostrarAgregarUsuario();
+			}
+		});
+	}
+	this.crearPartida=function(nick){
+		let cli=this;
+		$.getJSON("/crearPartida/"+nick,function(data){
+			//se ejecuta cuando conteste el servidor
+			console.log(data);
+			if (data.codigo!=-1){
+				console.log("Usuario "+cli.nick+" crea partida codigo: "+data.codigo)
+				//ws.nick=data.nick;
+				//$.cookie("nick",ws.nick);
+				//iu.mostrarHome(data);
+			}
+			else{
+				console.log("No se ha podido crear partida")
+				//iu.mostrarModal("El nick ya está en uso");
+				//iu.mostrarAgregarJugador();
+			}
+		});
+	}
+	this.unirseAPartida=function(nick,codigo){
 		let cli=this;
 		$.getJSON("/unirseAPartida/"+nick+"/"+codigo,function(data){
 			//se ejecuta cuando conteste el servidor
 			//console.log(data);
 			if (data.codigo!=-1){
-				console.log("Usuario "+nick+" se une a partida codigo: "+data.codigo)
+				console.log("Usuario "+cli.nick+" se une a partida codigo: "+data.codigo)
 				//ws.nick=data.nick;
 				//$.cookie("nick",ws.nick);
 				//iu.mostrarHome(data);
