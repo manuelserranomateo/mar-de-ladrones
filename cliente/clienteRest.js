d13b346340438c97c679f59dd77602abfeb96a3d
manuelserranomateo <manuelserranomateo@gmail.com>
2022-10-11 09:41:23 +0200

    Mostrar lista de partidas

--- cliente/clienteRest.js
+++ cliente/clienteRest.js
@@ -1,66 +1,69 @@
-function ClienteRest(){
-	this.nick;	
-	this.agregarUsuario=function(nick){
-		let cli=this;
-		$.getJSON("/agregarUsuario/"+nick,function(data){
-			//se ejecuta cuando conteste el servidor
-			console.log(data);
-			if (data.nick!=-1){
-				console.log("Usuario "+data.nick+" registrado")
-				cli.nick=data.nick;
-				//ws.nick=data.nick;
-				//$.cookie("nick",ws.nick);
-				iu.mostrarHome();
-				iu.mostrarCrearPartida();
-				iu.mostrarListaDePartidas(this.obtenerPartidas);
-			}
-			else{
-				console.log("No se ha podido registrar el usuario")
-				//iu.mostrarModal("El nick ya está en uso");
-				iu.mostrarAgregarUsuario();
-			}
-		});
-	}
-	this.crearPartida=function(nick){
-		let cli=this;
-		$.getJSON("/crearPartida/"+nick,function(data){
-			//se ejecuta cuando conteste el servidor
-			console.log(data);
-			if (data.codigo!=-1){
-				console.log("Usuario "+cli.nick+" crea partida codigo: "+data.codigo)
-				//ws.nick=data.nick;
-				//$.cookie("nick",ws.nick);
-				//iu.mostrarHome(data);
-			}
-			else{
-				console.log("No se ha podido crear partida")
-				//iu.mostrarModal("El nick ya está en uso");
-				//iu.mostrarAgregarJugador();
-			}
-		});
-	}
-	this.unirseAPartida=function(nick,codigo){
-		let cli=this;
-		$.getJSON("/unirseAPartida/"+nick+"/"+codigo,function(data){
-			//se ejecuta cuando conteste el servidor
-			//console.log(data);
-			if (data.codigo!=-1){
-				console.log("Usuario "+cli.nick+" se une a partida codigo: "+data.codigo)
-				//ws.nick=data.nick;
-				//$.cookie("nick",ws.nick);
-				//iu.mostrarHome(data);
-			}
-			else{
-				console.log("No se ha podido unir a partida")
-				//iu.mostrarModal("El nick ya está en uso");
-				//iu.mostrarAgregarJugador();
-			}
-		});
-	}
+function ClienteRest() {
+    this.nick;
+    this.agregarUsuario = function (nick) {
+        let cli = this;
+        $.getJSON("/agregarUsuario/" + nick, function (data) {
+            //se ejecuta cuando conteste el servidor
+            console.log(data);
+            if (data.nick != -1) {
+                console.log("Usuario " + data.nick + " registrado")
+                cli.nick = data.nick;
+                //ws.nick=data.nick;
+                //$.cookie("nick",ws.nick);
+                cli.obtenerPartidas();
+                iu.mostrarHome();
+            }
+            else {
+                console.log("No se ha podido registrar el usuario")
+                //iu.mostrarModal("El nick ya está en uso");
+                iu.mostrarAgregarUsuario();
+            }
+        });
+    }
+    this.crearPartida = function () {
+        let cli = this;
+        let nick = cli.nick;
+        $.getJSON("/crearPartida/" + nick, function (data) {
+            //se ejecuta cuando conteste el servidor
+            console.log(data);
+            if (data.codigo != -1) {
+                console.log("Usuario " + nick + " crea partida codigo: " + data.codigo);
+                iu.mostrarCodigo(data.codigo);
+                //ws.nick=data.nick;
+                //$.cookie("nick",ws.nick);
+                //iu.mostrarHome(data);
+            }
+            else {
+                console.log("No se ha podido crear partida")
+                //iu.mostrarModal("El nick ya está en uso");
+                //iu.mostrarAgregarJugador();
+            }
+        });
+    }
+    this.unirseAPartida = function (nick, codigo) {
+        let cli = this;
+        $.getJSON("/unirseAPartida/" + nick + "/" + codigo, function (data) {
+            //se ejecuta cuando conteste el servidor
+            //console.log(data);
+            if (data.codigo != -1) {
+                console.log("Usuario " + cli.nick + " se une a partida codigo: " + data.codigo)
+                //ws.nick=data.nick;
+                //$.cookie("nick",ws.nick);
+                //iu.mostrarHome(data);
+            }
+            else {
+                console.log("No se ha podido unir a partida")
+                //iu.mostrarModal("El nick ya está en uso");
+                //iu.mostrarAgregarJugador();
+            }
+        });
+    }
 
-// 	this.obtenerPartidas =function(){
-// 		$.getJSON("/obtenerPartidas/",function(data){
-// 			//console.log(data);
-// 		});
-// }
+    this.obtenerPartidas = function () {
+        let cli = this;
+        $.getJSON("/obtenerPartidasDisponibles/", function (lista) {
+            // console.log(lista);
+            iu.mostrarListaDePartidas(lista);
+        });
+    }
 }
--- cliente/controlWeb.js
+++ cliente/controlWeb.js
@@ -1,27 +1,28 @@
-function ControlWeb(){
-	this.mostrarAgregarUsuario=function(){
-		let cadena= '<div class="row" id="mAU">';//'<form class="form-row needs-validation"  id="mAJ">';
-		cadena=cadena+'<div class="row"><h2>El juego indefinido</h2></div>';
-		cadena=cadena+'<div class="row">';
-		cadena=cadena+'<div class="col">'
-        cadena=cadena+'<input type="text" class="form-control mb-2 mr-sm-2" id="usr" placeholder="Introduce tu nick (max 6 letras)" required></div>';
-        cadena=cadena+'<div class="col">';
-        cadena=cadena+'<button id="btnAU" class="btn btn-primary mb-2 mr-sm-2">Iniciar sesión</button>';
-        //cadena=cadena+'<a href="/auth/google" class="btn btn-primary mb-2 mr-sm-2">Accede con Google</a>';
-        cadena=cadena+'</div>'; //' </form>';
-        cadena=cadena+'<div id="nota"></div>';
-        cadena=cadena+'</div></div>';
+function ControlWeb() {
+	this.mostrarAgregarUsuario = function () {
+		let cadena = '<div class="row" id="mAU">';//'<form class="form-row needs-validation"  id="mAJ">';
+		cadena = cadena + "<div class='col'>"
+		cadena = cadena + '<div class="row"><div class="col"><h2>El juego indefinido</h2></div></div>';
+		cadena = cadena + '<div class="row">';
+		cadena = cadena + '<div class="col">'
+		cadena = cadena + '<input type="text" class="form-control mb-2 mr-sm-2" id="usr" placeholder="Introduce tu nick (max 6 letras)" required></div>';
+		cadena = cadena + '<div class="col">';
+		cadena = cadena + '<button id="btnAU" class="btn btn-primary mb-2 mr-sm-2">Iniciar sesión</button>';
+		//cadena=cadena+'<a href="/auth/google" class="btn btn-primary mb-2 mr-sm-2">Accede con Google</a>';
+		cadena = cadena + '</div>'; //' </form>';
+		cadena = cadena + '<div id="nota"></div>';
+		cadena = cadena + '</div></div>';
 
-		$("#agregarUsuario").append(cadena);     
+		$("#agregarUsuario").append(cadena);
 		//$("#nota").append("<div id='aviso' style='text-align:right'>Inicia sesión con Google para jugar</div>");    
 
-		$("#btnAU").on("click",function(e){
-			if ($('#usr').val() === '' || $('#usr').val().length>6) {
-			    e.preventDefault();
-			    $('#nota').append('Nick inválido');
+		$("#btnAU").on("click", function (e) {
+			if ($('#usr').val() === '' || $('#usr').val().length > 6) {
+				e.preventDefault();
+				$('#nota').append('Nick inválido');
 			}
-			else{
-				var nick=$('#usr').val();
+			else {
+				var nick = $('#usr').val();
 				$("#mAU").remove();
 				//$("#aviso").remove();
 				rest.agregarUsuario(nick);
@@ -30,39 +31,57 @@ function ControlWeb(){
 		})
 	}
 
-	this.mostrarHome=function(){
+	this.mostrarHome = function () {
 		$('#mH').remove();
-		let cadena="<div id='mH'>";
-		cadena=cadena+"<p>Bienvenido "+rest.nick+"</p>";
-		cadena=cadena+"</div>";
+		let cadena = "<div class ='row' id='mH'>";
+		cadena = cadena + "<div class='col'>";
+		cadena = cadena + "<p>Bienvenido " + rest.nick + "</p>";
+		cadena = cadena + "<div id='codigo'></div>"
+		cadena = cadena + "</div></div>";
 		$('#agregarUsuario').append(cadena);
+		this.mostrarCrearPartida();
 	}
 
-	this.mostrarCrearPartida=function(){
+	this.mostrarCrearPartida = function () {
 		//dibujar un boton, que al hacer click llame a crear
 		//partida de rest
-
-        let cadena= '<div class="row" id="mCP">';
-        cadena=cadena+'<button id="btnCP" class="btn btn-primary mb-2 mr-sm-2">Crear partida</button>';
-        cadena=cadena+'</div>';
-        cadena=cadena+'<div id="nota"></div>';
-        cadena=cadena+'</div></div>';
+		$("mCP").remove();
+		let cadena = '<div class="row" id="mCP">';
+		cadena = cadena + '<button id="btnCP" class="btn btn-primary mb-2 mr-sm-2">Crear partida</button>';
+		cadena = cadena + '</div>';
+		cadena = cadena + '<div id="nota"></div>';
+		cadena = cadena + '</div></div>';
 
 		$("#crearPartida").append(cadena);
-        
-        $("#btnCP").on("click",function(){
-				rest.crearPartida(rest.nick);
-				$('#nota').append('Partida ' + rest.codigo + ' creada satisfactoriamente por ' + rest.nick);
+
+		$("#btnCP").on("click", function () {
+			$("#mCP").remove();
+			rest.crearPartida();
 		})
 	}
 
-	this.mostrarListaDePartidas=function(lista){
+	this.mostrarCodigo = function (codigo) {
+		let cadena = "Codigo de la partida: " + codigo;
+		$("#codigo").append(cadena)
+	}
+
+	this.mostrarListaDePartidas = function (lista) {
 		//crear un control visual tipo lista para mostrar
 		//la lista de partidas
 		//y permitir unirse con un click
-		console.log(lista);
-	    let cadena= '<h4>Lista de partidas disponibles</h4>';
+		$('mLP').remove();
+		let cadena = '<div id="mLP">';
+		cadena = cadena + '<div class="row">';
+		cadena = cadena + '<ul class="list-group">';
+		for (i = 0; i < lista.length; i++) {
+			cadena = cadena + "<li class='list-group-item'>" + lista[i].codigo + " creada por " + lista[i].owner + "</li>";
+			cadena = cadena + '<button id="btnUP" class="btn btn-primary mb-2 mr-sm-2">Unirse</button>';
+
+		}
+		cadena = cadena + " </ul>";
+		cadena = cadena + " </div></div>";
 		$("#listaPartidas").append(cadena);
+
 
 	}
 }
