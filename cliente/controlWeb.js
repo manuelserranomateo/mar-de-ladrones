74ecb0810cd38b79d058752f5c468c2c8f804c9b
manuelserranomateo <manuelserranomateo@gmail.com>
2022-12-22 17:56:53 +0100

    Refactoring visual

--- cliente/controlWeb.js
+++ cliente/controlWeb.js
@@ -9,18 +9,21 @@ function ControlWeb() {
 	}
 
 	this.mostrarAgregarUsuario = function () {
-		let cadena = '<div class="row" id="mAU">';
-		cadena = cadena + "<div class='col'>"
-		cadena = cadena + '<div class="row"><div class="col"><h1>Mar de Ladrones</h1></div></div>';
-		cadena = cadena + '<div class="row" style="margin-top:30px">';
-		cadena = cadena + '<div class="col">'
-		cadena = cadena + '<input type="text" class="form-control mb-2 mr-sm-2 text-center w-75" id="usr" placeholder="Introduce tu nick (max 6 letras)" required></div>';
-		cadena = cadena + '<div class="col">';
-		cadena = cadena + '<button id="btnAU" class="bn632-hover bn19">Iniciar sesion</button></a>';
-		cadena = cadena + '<a class="bn632-hover bn19" href="/auth/google">Accede con Google</a>';
-		cadena = cadena + '</div>';
-		cadena = cadena + '<div id="nota"></div>';
-		cadena = cadena + '</div></div>';
+		let cadena = '<div class="text-center" id="mAU">';
+		cadena = cadena + '<h1>Mar de Ladrones</h1>';
+		cadena = cadena + '<div>'
+		cadena = cadena + '<input type="text" id="usr" placeholder="Introduce tu nick" required>'
+		cadena = cadena + '<button id="btnAU" class="bn632-hover bn19">Iniciar sesion</button>'
+		cadena = cadena + '</div>'
+		cadena = cadena + '<a href="/auth/google"><button class="bn632-hover bn19">Accede con Google'
+		// SVG con el logo de Google
+		cadena = cadena + '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-google" viewBox="0 0 16 16"> <path d="M15.545 6.558a9.42 9.42 0 0 1 .139 1.626c0 2.434-.87 4.492-2.384 5.885h.002C11.978 15.292 10.158 16 8 16A8 8 0 1 1 8 0a7.689 7.689 0 0 1 5.352 2.082l-2.284 2.284A4.347 4.347 0 0 0 8 3.166c-2.087 0-3.86 1.408-4.492 3.304a4.792 4.792 0 0 0 0 3.063h.003c.635 1.893 2.405 3.301 4.492 3.301 1.078 0 2.004-.276 2.722-.764h-.003a3.702 3.702 0 0 0 1.599-2.431H8v-3.08h7.545z"/> </svg>'
+		cadena = cadena + '</button></a>';
+		cadena = cadena + '<div><a><button class="bn632-hover bn19">Accede con Twitter'
+		// SVG con el logo de Twitter
+		cadena = cadena + '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-twitter" viewBox="0 0 16 16"> <path d="M5.026 15c6.038 0 9.341-5.003 9.341-9.334 0-.14 0-.282-.006-.422A6.685 6.685 0 0 0 16 3.542a6.658 6.658 0 0 1-1.889.518 3.301 3.301 0 0 0 1.447-1.817 6.533 6.533 0 0 1-2.087.793A3.286 3.286 0 0 0 7.875 6.03a9.325 9.325 0 0 1-6.767-3.429 3.289 3.289 0 0 0 1.018 4.382A3.323 3.323 0 0 1 .64 6.575v.045a3.288 3.288 0 0 0 2.632 3.218 3.203 3.203 0 0 1-.865.115 3.23 3.23 0 0 1-.614-.057 3.283 3.283 0 0 0 3.067 2.277A6.588 6.588 0 0 1 .78 13.58a6.32 6.32 0 0 1-.78-.045A9.344 9.344 0 0 0 5.026 15z"/> </svg>'
+		cadena = cadena + '</button></a>'
+		cadena = cadena + '</div>'
 
 		$("#agregarUsuario").append(cadena);
 
